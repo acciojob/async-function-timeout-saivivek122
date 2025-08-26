f758a63592abe38274f7ec03ccfb1f3dfaa7c41c
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2025-08-26 06:31:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+let textInput=document.getElementById("text");
+let delayInput=document.getElementById("delay");
+let submitBtn=document.getElementById("btn");
+let outputContent=document.getElementById("output");
+
+
+function timer(time){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve(time)
+
+        },time)
+    })
+    
+}
+
+submitBtn.addEventListener("click",async()=>{
+
+    let textInputValue=textInput.value;
+    let delayInputValue=Number(delayInput.value);
+
+    await timer(delayInputValue);
+    outputContent.textContent=textInputValue;
+
+    
+})
